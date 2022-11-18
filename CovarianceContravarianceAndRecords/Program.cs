6628c59ca0f2538e693a620041416b8b8274a426
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-18 11:09:00 +0100

    Add project files.

---
+++ CovarianceContravarianceAndRecords/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CovarianceContravarianceAndRecords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
