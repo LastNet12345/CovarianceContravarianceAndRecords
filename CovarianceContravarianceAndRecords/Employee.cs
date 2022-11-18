61d8a66f4d94411f3c76fc7b5a9c2ed176e8c21e
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-18 11:40:01 +0100

    Array and List

---
+++ CovarianceContravarianceAndRecords/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianceContravarianceAndRecords
+{
+    internal class Employee
+    {
+    }
+
+    internal class Admin : Employee { }
+    internal class Manager : Employee { }
+}
--- CovarianceContravarianceAndRecords/Program.cs
+++ CovarianceContravarianceAndRecords/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Covariance
+            ArrayExample();
+            ListExample();
+        }
+
+        private static void ListExample()
+        {
+            //2.
+           // List<Employee> list = new List<Admin>();
+            //list.Add(new Manager());
+
+            //3.
+            IEnumerable<Employee> list2 = new List<Admin>();
+        }
+
+        private static void ArrayExample()
+        {
+            //1.
+            Employee[] employees = new Admin[] { new Admin(), new Admin(), new Admin(), new Admin() };
+            employees[0] = new Employee();
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item);
+            } 
+            
+            //4.
+            IEnumerable<Employee> employees2 = new Admin[] { new Admin(), new Admin(), new Admin(), new Admin() };
+            employees[0] = new Employee();
+
+            foreach (var item in employees)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
