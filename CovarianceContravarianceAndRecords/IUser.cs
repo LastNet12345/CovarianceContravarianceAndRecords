16afba7c492e34c0cf3480faa3c628635a8d9848
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-18 11:48:38 +0100

    out keyword

---
+++ CovarianceContravarianceAndRecords/IUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianceContravarianceAndRecords;
+
+internal interface IUser<out T>
+{
+    public T Get(int id);
+}
+
+internal class SuperUser<T> : IUser<T>
+{
+    public T Get(int id)
+    {
+        return Activator.CreateInstance<T>();
+    }
+}
--- CovarianceContravarianceAndRecords/Program.cs
+++ CovarianceContravarianceAndRecords/Program.cs
@@ -1,43 +1,51 @@
-﻿namespace CovarianceContravarianceAndRecords
+﻿namespace CovarianceContravarianceAndRecords;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Covariance
+        ArrayExample();
+        ListExample();
+        OwnImplementation();
+    }
+
+    private static void OwnImplementation()
+    {
+        //5.
+       // SuperUser<Employee> superUser = new SuperUser<Admin>();
+        
+        IUser<Employee> superUser = new SuperUser<Admin>();
+    }
+
+    private static void ListExample()
+    {
+        //2.
+       // List<Employee> list = new List<Admin>();
+        //list.Add(new Manager());
+
+        //3.
+        IEnumerable<Employee> list2 = new List<Admin>();
+    }
+
+    private static void ArrayExample()
+    {
+        //1.
+        //Employee[] employees = new Admin[] { new Admin(), new Admin(), new Admin(), new Admin() };
+        //employees[0] = new Manager();
+
+        //foreach (var item in employees)
+        //{
+        //    Console.WriteLine(item);
+        //} 
+        
+        //4.
+        IEnumerable<Employee> employees2 = new Admin[] { new Admin(), new Admin(), new Admin(), new Admin() };
+       // employees2[0] = new Employee();
+
+        foreach (var item in employees2)
         {
-            //Covariance
-            ArrayExample();
-            ListExample();
-        }
-
-        private static void ListExample()
-        {
-            //2.
-           // List<Employee> list = new List<Admin>();
-            //list.Add(new Manager());
-
-            //3.
-            IEnumerable<Employee> list2 = new List<Admin>();
-        }
-
-        private static void ArrayExample()
-        {
-            //1.
-            //Employee[] employees = new Admin[] { new Admin(), new Admin(), new Admin(), new Admin() };
-            //employees[0] = new Manager();
-
-            //foreach (var item in employees)
-            //{
-            //    Console.WriteLine(item);
-            //} 
-            
-            //4.
-            IEnumerable<Employee> employees2 = new Admin[] { new Admin(), new Admin(), new Admin(), new Admin() };
-           // employees2[0] = new Employee();
-
-            foreach (var item in employees2)
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(item);
         }
     }
 }
